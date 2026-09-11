12f62505520b3de6fc339daece03f96a09772096
Nostag <gastonlions.pro@gmail.com>
2026-09-11 17:01:29 +0200

    Continuer les exercices pour maitriser les bases de react + crow

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,13 +1,50 @@
 #include <crow.h>
+#include <crow/middlewares/cors.h>
 
-int	main()
+int main()
 {
-	crow::SimpleApp app;
+    crow::App<crow::CORSHandler> app;
 
-	CROW_ROUTE(app, "/")
+    auto& cors = app.get_middleware<crow::CORSHandler>();
+
+    cors.global()
+        .origin("http://localhost:5173");
+
+    CROW_ROUTE(app, "/")
+    ([] {
+        return "Gaston\n";
+    });
+
+	CROW_ROUTE(app, "/api/profil/")
+	.methods(crow::HTTPMethod::GET, crow::HTTPMethod::PATCH)
 	([] {
-		return "Hello from Crow !";
+		crow::json::wvalue data;
+		data["name"] = "Gaston";
+		data["city"] = "Paris";
+		data["age"] = 27;
+		data["bio"] = "Je suis une description";
+		return (data);
 	});
 
-	app.port(18080).multithreaded().run();
+	CROW_ROUTE(app, "/api/profil/")
+	.methods(crow::HTTPMethod::POST)
+	([](const crow::request& req)
+	{
+		auto body = crow::json::load(req.body);
+		if (!body)
+			return crow::response(400, "Invalid JSON");
+		std::string name = body["name"].s();
+		int age = body["age"].i();
+		std::string city = body["city"].s();
+		std::string bio = body["bio"].s();
+
+		std::cout << "Name : " << name << std::endl;
+		std::cout << "Age : " << age << std::endl;
+		std::cout << "City : " << city << std::endl;
+		std::cout << "Bio : " << bio << std::endl;
+
+		return crow::response(201, "Datas modified");
+	});
+
+    app.port(18080).multithreaded().run();
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,87 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
+interface DataProfilProps
+{
+  name: string;
+  age: number;
+  city: string;
+  bio: string;
+}
+
+function Button()
+{
+  return (
+    <>
+      <button>Voir mon profil</button>
+    </>
+  )
+}
+
+function Save()
+{
+  function handleSubmit(e)
+  {
+    e.preventDefault();
+    const form = e.target;
+    const formData = FormData(form);
+    const name = formData.get("name");
+    const age = formData.get("age");
+    const city = formData.get("location");
+    const bio = formData.get("bio");
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({name:{name}, age:{age}, city:{city}, bio:{bio}})
+    };
+    fetch('http://localhost:18080/api/profil', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log(data.message));
+  }
+}
+
+function MyProfil({name, age, city, bio}: DataProfilProps)
+{
 
   return (
     <>
-      <h1>Matcha</h1>
+      <div>
+        <form action={Save}>
+          <input name="name" defaultValue={name}/>
+          <input name="age" defaultValue={age}/>
+          <input name="location" defaultValue={city}/>
+          <input name="bio" defaultValue={bio}/>
+          <button type="submit">Save</button>
+        </form>
+      </div>
     </>
   )
+}
+
+function App() {
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
+  const [city, setCity] = useState<string>("");
+  const [bio, setBio] = useState<string>("");
+
+  useEffect(() => {
+    fetch("http://localhost:18080/api/profil")
+    .then((response) => response.json())
+    .then((data) => {
+      setName(data["name"]);
+      setCity(data["city"]);
+      setAge(data["age"]);
+      setBio(data["bio"]);
+    })
+  }, [])
+
+  return (
+    <>
+      <MyProfil name={name} city={city} age={age} bio={bio}/>
+      <Button/>
+    </>
+  );
 }
 
 export default App
